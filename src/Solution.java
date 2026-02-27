7cc9219bf693b0edbbb765283d756de81106e527
devanandana21 <tve24el018@cet.ac.in>
2026-02-27 10:57:41 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -6,13 +6,17 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // TODO: Read the integer N
+        int n = scanner.nextInt();
         
-        // TODO: Create a HashSet to store the integers
+        Set<Integer> numbers = new HashSet<>();
         
-        // TODO: Loop N times to read the integers and add them to the HashSet
+        for (int i = 0; i < n; i++) {
+            numbers.add(scanner.nextInt());
+        }
         
-        // TODO: Iterate through the HashSet and print the unique numbers (space-separated)
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
         
         scanner.close();
     }
